683f89e8feec711674e2b4c2a02df09df129cdbe
Mykyta Reutov <liljollier@gmail.com>
2023-01-22 20:06:57 +0200

    use ssl

--- typeOrm.config.ts
+++ typeOrm.config.ts
@@ -11,7 +11,7 @@ export default new DataSource({
   password: process.env.DB_PASS,
   database: process.env.DB_DATABASE,
   ssl: {
-    cert: process.env.SSL_CERT,
+    ca: process.env.SSL_CERT,
   },
   entities: ['dist/src/core/entities/**/*{.js,.ts}'],
   migrations: ['dist/migrations/**/*{.js,.ts}'],
