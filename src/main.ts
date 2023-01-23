b343c5c3f0a8c9bda90037e17005af0ee1bf4eda
Mykyta Reutov <liljollier@gmail.com>
2023-01-24 00:24:16 +0200

    add port for app from env

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import {json, urlencoded} from "express";
+import { json, urlencoded } from 'express';
+import * as process from 'process';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
 
   const config = new DocumentBuilder()
-  .setTitle('Cats example')
-  .setDescription('The cats API description')
-  .setVersion('1.0')
-  .addTag('cats')
-  .build();
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  
-  await app.listen(3000);
+
+  await app.listen(parseInt(process.env.HTTP_PORT) || 3000);
 }
 bootstrap();
